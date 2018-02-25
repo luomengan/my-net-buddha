53b77e4fd7a83b36a964a0a3654463dcc893e689
luomengan <493671072@qq.com>
2018-02-25 21:53:12 +0800

    do

--- net-buddha/outresources/front/js/music/musicList.js
+++ net-buddha/outresources/front/js/music/musicList.js
@@ -2,7 +2,7 @@ var musicList = [
     {
         title : '笑傲江湖',
 		singer : '许冠杰',
-		cover  : 'http://p1.music.126.net/3fipbbBNProjPmgeVW623Q==/7885697394815923.jpg',
-		src    : 'http://m2.music.126.net/z5t-DBv6ugHWN8BCGynjSw==/7715273092477776.mp3'
+		cover  : 'http://luomengan.com:9090/net-buddha/musicFile/music-cover.jpg',
+		src    : 'http://luomengan.com:9090/net-buddha/musicFile/1.mp3'
     }
 ];
